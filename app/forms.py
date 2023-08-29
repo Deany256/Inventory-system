f77345f842c4606f3bc32bf72d5cc86e7e71af5e
Deany256 <gdeandude@gmail.com>
2023-08-29 22:29:38 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,12 @@ class ItemForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired()])
     # Add more form fields as needed
     submit = SubmitField('Submit')
+
+class AddItemForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    category_id = IntegerField('Category ID', validators=[DataRequired()])
+    supplier_id = IntegerField('Supplier ID', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    submit = SubmitField('Add Item')
