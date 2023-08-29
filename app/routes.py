d4f8e140a796f3db60bd4ab96a92e2b39de0a57a
Deany256 <gdeandude@gmail.com>
2023-08-29 22:29:45 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,29 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from app.models import Item, Category, Supplier, Order, Sale
+from app.forms import AddItemForm
 
 @app.route('/')
 def index():
     items = Item.query.all()
     # Add more logic as needed
     return render_template('index.html', items=items)
+
+@app.route('/add_item', methods=['GET', 'POST'])
+def add_item():
+    form = AddItemForm()
+    if form.validate_on_submit():
+        category = Category.query.get(form.category_id.data)
+        supplier = Supplier.query.get(form.supplier_id.data)
+        item = Item(name=form.name.data,
+                    description=form.description.data,
+                    category=category,
+                    supplier=supplier,
+                    price=form.price.data,
+                    quantity=form.quantity.data)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_item.html', form=form)
 
 # Define other routes and views as needed
